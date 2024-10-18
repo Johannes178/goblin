9bbaac68006f537f3b9a8ba593fbde416fcbd96e
Johannes178 <johannes.jokinen@live.com>
2024-10-18 23:41:05 +0300

    teema

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import AbstractAnimation from "./components/AbstractAnimation";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>JokiDev</h1>
-      </header>
-      <main>
-        <Canvas>
-          <OrbitControls />
-          <ambientLight intensity={0.5} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-          <AbstractAnimation />
-        </Canvas>
-      </main>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas>
+        <AbstractAnimation />
+      </Canvas>
     </div>
   );
 }
